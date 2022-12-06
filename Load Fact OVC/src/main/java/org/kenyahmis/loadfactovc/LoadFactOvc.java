cd6db81406a540129c20415601628f53dd15285b
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-12-06 17:21:09 +0300

    Add Fact OVC table

---
+++ Load Fact OVC/src/main/java/org/kenyahmis/loadfactovc/LoadFactOvc.java
@@ -0,0 +1,162 @@
+package org.kenyahmis.loadfactovc;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.*;
+import org.apache.spark.storage.StorageLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+public class LoadFactOvc {
+    private static final Logger logger = LoggerFactory.getLogger(LoadFactOvc.class);
+    public static void main(String[] args) {
+
+        SparkConf conf = new SparkConf();
+        conf.setAppName("Load OVC Fact");
+        SparkSession session = SparkSession.builder()
+                .config(conf)
+                .getOrCreate();
+        RuntimeConfig rtConfig = session.conf();
+
+        Dataset<Row> dimMFLPartnerAgencyCombinationDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.ods.url"))
+                .option("driver", rtConfig.get("spark.ods.driver"))
+                .option("user", rtConfig.get("spark.ods.user"))
+                .option("password", rtConfig.get("spark.ods.password"))
+                .option("query", "select MFL_Code,SDP,[SDP Agency] as Agency from dbo.All_EMRSites")
+                .load();
+        dimMFLPartnerAgencyCombinationDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimMFLPartnerAgencyCombinationDataFrame.createOrReplaceTempView("MFL_partner_agency_combination");
+
+        final String loadSourceOVCQueryFileName = "SourceOVC.sql";
+        LoadFactOvc loadFactOvc = new LoadFactOvc();
+        String loadSourceOVCQuery = loadFactOvc.loadQuery(loadSourceOVCQueryFileName);
+
+        Dataset<Row> sourceOvcDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.ods.url"))
+                .option("driver", rtConfig.get("spark.ods.driver"))
+                .option("user", rtConfig.get("spark.ods.user"))
+                .option("password", rtConfig.get("spark.ods.password"))
+                .option("query", loadSourceOVCQuery)
+                .load();
+        sourceOvcDataFrame.createOrReplaceTempView("source_ovc");
+        sourceOvcDataFrame.persist(StorageLevel.DISK_ONLY());
+        sourceOvcDataFrame.printSchema();
+
+        Dataset<Row> dimDateDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.dimDate.dbtable"))
+                .load();
+        dimDateDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimDateDataFrame.createOrReplaceTempView("DimDate");
+
+        Dataset<Row> dimFacilityDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.dimFacility.dbtable"))
+                .load();
+        dimFacilityDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimFacilityDataFrame.createOrReplaceTempView("Dimfacility");
+
+        Dataset<Row> dimPatientDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.dimPatient.dbtable"))
+                .load();
+        dimPatientDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimPatientDataFrame.createOrReplaceTempView("DimPatient");
+
+        Dataset<Row> dimPartnerDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.dimPartner.dbtable"))
+                .load();
+        dimPartnerDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimPartnerDataFrame.createOrReplaceTempView("DimPartner");
+
+        Dataset<Row> dimAgencyDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.dimAgency.dbtable"))
+                .load();
+        dimAgencyDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimAgencyDataFrame.createOrReplaceTempView("DimAgency");
+
+        Dataset<Row> dimAgeGroupDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.dimAgeGroup.dbtable"))
+                .load();
+        dimAgeGroupDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimAgeGroupDataFrame.createOrReplaceTempView("DimAgeGroup");
+
+        Dataset<Row> dimRelationshipWithPatientDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.dimRelationshipWithPatient.dbtable"))
+                .load();
+        dimRelationshipWithPatientDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimRelationshipWithPatientDataFrame.createOrReplaceTempView("DimRelationshipWithPatient");
+
+        String factOvcQuery = loadFactOvc.loadQuery("LoadFactOvc.sql");
+        Dataset<Row> factOvcDf = session.sql(factOvcQuery);
+        factOvcDf.printSchema();
+        final int writePartitions = 20;
+        factOvcDf
+                .repartition(writePartitions)
+                .write()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", rtConfig.get("spark.factOvc.dbtable"))
+                .option("truncate", "true")
+                .mode(SaveMode.Overwrite)
+                .save();
+    }
+
+    private String loadQuery(String fileName) {
+        String query;
+        InputStream inputStream = LoadFactOvc.class.getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            logger.error(fileName + " not found");
+            throw new RuntimeException(fileName + " not found");
+        }
+        try {
+            query = IOUtils.toString(inputStream, Charset.defaultCharset());
+        } catch (IOException e) {
+            logger.error("Failed to load query from file", e);
+            throw new RuntimeException("Failed to load query from file");
+        }
+        return query;
+    }
+}
---
+++ Load Fact OVC/src/main/resources/SourceOVC.sql
@@ -0,0 +1,17 @@
+select
+    distinct ovc.PatientPK,
+             ovc.PatientID,
+             ovc.SiteCode,
+             OVCEnrollmentDate,
+             RelationshipToClient,
+             EnrolledinCPIMS,
+             CPIMSUniqueIdentifier,
+             PartnerOfferingOVCServices,
+             OVCExitReason,
+             ExitDate,
+             datediff(yy, patient.DOB, last_encounter.LastEncounterDate) as AgeLastVisit
+from dbo.CT_OVC as ovc
+         left join dbo.Intermediate_LastPatientEncounter as last_encounter on last_encounter.PatientPK = ovc.PatientPK
+    and last_encounter.SiteCode = ovc.SiteCode
+         left join dbo.CT_Patient as patient on patient.PatientPK = ovc.PatientPK
+    and patient.SiteCode = ovc.SiteCode
