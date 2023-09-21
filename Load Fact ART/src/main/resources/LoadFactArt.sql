82a177ef8e4c47f779e1b2089f02a6a6b4551404
cbrianbet <cbrianbet@outlook.com>
2023-09-21 11:17:35 +0300

    Updated FACTART queries

--- Load Fact ART/src/main/resources/LoadFactArt.sql
+++ Load Fact ART/src/main/resources/LoadFactArt.sql
@@ -4,7 +4,7 @@ Select
     partner.PartnerKey,
     agency.AgencyKey,
     age_group.AgeGroupKey,
-    StartARTDate.Date As StartARTDateKey,
+    StartARTDate.DateKey As StartARTDateKey,
     LastARTDate.DateKey  as LastARTDateKey,
     ARTOutcome.ARTOutcomeKey,
     lastRegimen As CurrentRegimen,
@@ -15,11 +15,9 @@ Select
     AgeAtARTStart,
     AgeLastVisit,
     CASE
-        WHEN floor( AgeLastVisit ) < 15 THEN
-            'Child'
-        WHEN floor( AgeLastVisit ) >= 15 THEN
-            'Adult' ELSE 'Aii'
-        END AS Agegrouping,
+        WHEN floor( AgeLastVisit ) < 15 THEN 'Child'
+        WHEN floor( AgeLastVisit ) >= 15 THEN 'Adult' ELSE 'Aii'
+    END AS Agegrouping,
     TimetoARTDiagnosis,
     TimetoARTEnrollment,
     PregnantARTStart,
@@ -31,13 +29,13 @@ Select
     PreviousARTRegimen,
     current_date() as LoadDate
 from Patient
-    left join DimPatient as Pat on pat.PatientPKHash=Patient.PatientPkHash and pat.SiteCode=Patient.SiteCode
-    left join Dimfacility fac on fac.MFLCode=Patient.SiteCode
-    left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = Patient.SiteCode
-    left join DimPartner as partner on partner.PartnerName = upper(MFL_partner_agency_combination.SDP)
-    left join DimAgeGroup as age_group on age_group.Age = Patient.AgeLastVisit
-    left join DimDate as StartARTDate on StartARTDate.Date= Patient.StartARTDate
-    left join DimDate as LastARTDate on  LastARTDate.Date=Patient.LastARTDate
-    left join DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
-    left join Intermediate_ARTOutcomes As IOutcomes  on IOutcomes.PatientPKHash = Patient.PatientPkHash  and IOutcomes.SiteCode = Patient.SiteCode
-    left join DimARTOutcome ARTOutcome on ARTOutcome.ARTOutcome=IOutcomes.ARTOutcome;
+left join DimPatient as Pat on pat.PatientPKHash=Patient.PatientPkHash and Pat.SiteCode=Patient.SiteCode
+left join Dimfacility fac on fac.MFLCode=Patient.SiteCode
+left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code  = Patient.SiteCode
+left join DimPartner as partner on partner.PartnerName = MFL_partner_agency_combination.SDP
+left join DimAgeGroup as age_group on age_group.Age = Patient.AgeLastVisit
+left join DimDate as StartARTDate on StartARTDate.Date = Patient.StartARTDate
+left join DimDate as LastARTDate on  LastARTDate.Date=Patient.LastARTDate
+left join DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
+left join Intermediate_ARTOutcomes As IOutcomes  on IOutcomes.PatientPKHash = Patient.PatientPkHash  and IOutcomes.SiteCode = Patient.SiteCode
+left join DimARTOutcome ARTOutcome on ARTOutcome.ARTOutcome=IOutcomes.ARTOutcome;
--- Load Fact ART/src/main/resources/LoadPatient.sql
+++ Load Fact ART/src/main/resources/LoadPatient.sql
@@ -2,35 +2,40 @@ Select
     Patient.PatientIDHash,
     Patient.PatientPKHash,
     cast (Patient.SiteCode as nvarchar) As SiteCode,
-    DATEDIFF(yy,Patient.DOB,Patient.RegistrationAtCCC) AgeAtEnrol,
-    DATEDIFF(yy,Patient.DOB,ART.StartARTDate) AgeAtARTStart,
+    DATEDIFF(
+            yy, Patient.DOB, Patient.RegistrationAtCCC
+        ) AgeAtEnrol,
+    DATEDIFF(
+            yy, Patient.DOB, ART.StartARTDate
+        ) AgeAtARTStart,
     ART.StartARTAtThisfacility,
     ART.PreviousARTStartDate,
     ART.PreviousARTRegimen,
     ART.StartARTDate,
     LastARTDate,
-    CASE WHEN [DateConfirmedHIVPositive] IS NOT NULL AND ART.StartARTDate IS NOT NULL
-    THEN CASE WHEN DateConfirmedHIVPositive<= ART.StartARTDate THEN DATEDIFF(DAY,DateConfirmedHIVPositive,ART.StartARTDate)
-					ELSE NULL END
-				ELSE NULL END AS TimetoARTDiagnosis,
-      CASE WHEN Patient.RegistrationAtCCC IS NOT NULL AND ART.StartARTDate IS NOT NULL
-				THEN CASE WHEN Patient.RegistrationAtCCC<=ART.StartARTDate  THEN DATEDIFF(DAY,Patient.[RegistrationAtCCC],ART.StartARTDate)
-				ELSE NULL END
-				ELSE NULL END AS TimetoARTEnrollment,
-        Pre.PregnantARTStart,
-        Pre.PregnantAtEnrol,
-        las.LastEncounterDate As LastVisitDate,
-        las.NextAppointmentDate,
-        datediff(yy, patient.DOB, las.LastEncounterDate) as AgeLastVisit,
-        lastRegimen,
-        StartRegimen,
-        lastRegimenline,
-        StartRegimenline,
-        outcome.ARTOutcome
-
-from
-dbo.CT_Patient Patient
-inner join dbo.CT_ARTPatients ART on ART.PatientPK=Patient.Patientpk and ART.SiteCode=Patient.SiteCode
-left join dbo.Intermediate_PregnancyAsATInitiation Pre on Pre.Patientpk= Patient.PatientPK and Pre.SiteCode=Patient.SiteCode
-left join dbo.Intermediate_LastPatientEncounter las on las.PatientPK =Patient.PatientPK  and las.SiteCode =Patient.SiteCode
-left join dbo.Intermediate_ARTOutcomes outcome on outcome.PatientPK=Patient.PatientPK and outcome.SiteCode=Patient.SiteCode
+    CASE WHEN [DateConfirmedHIVPositive] IS NOT NULL
+        AND ART.StartARTDate IS NOT NULL THEN CASE WHEN DateConfirmedHIVPositive <= ART.StartARTDate THEN DATEDIFF(
+            DAY, DateConfirmedHIVPositive, ART.StartARTDate
+        ) ELSE NULL END ELSE NULL END AS TimetoARTDiagnosis,
+    CASE WHEN Patient.RegistrationAtCCC IS NOT NULL
+        AND ART.StartARTDate IS NOT NULL THEN CASE WHEN Patient.RegistrationAtCCC <= ART.StartARTDate THEN DATEDIFF(
+            DAY, Patient.[RegistrationAtCCC],
+            ART.StartARTDate
+        ) ELSE NULL END ELSE NULL END AS TimetoARTEnrollment,
+    Pre.PregnantARTStart,
+    Pre.PregnantAtEnrol,
+    las.LastEncounterDate As LastVisitDate,
+    las.NextAppointmentDate,
+    datediff(
+            yy, patient.DOB, las.LastEncounterDate
+        ) as AgeLastVisit,
+    lastRegimen,
+    StartRegimen,
+    lastRegimenline,
+    StartRegimenline,
+    outcome.ARTOutcome
+FROM ODS.dbo.CT_Patient Patient
+inner join ODS.dbo.CT_ARTPatients ART on ART.PatientPK = Patient.Patientpk and ART.SiteCode = Patient.SiteCode
+left join ODS.dbo.Intermediate_PregnancyAsATInitiation Pre on Pre.Patientpk = Patient.PatientPK and Pre.SiteCode = Patient.SiteCode
+left join ODS.dbo.Intermediate_LastPatientEncounter las on las.PatientPK = Patient.PatientPK and las.SiteCode = Patient.SiteCode
+left join ODS.dbo.Intermediate_ARTOutcomes outcome on outcome.PatientPK = Patient.PatientPK and outcome.SiteCode = Patient.SiteCode
