cb32b7082c07b64b78dd8ccf24cb437dec5a1246
cbrianbet <cbrianbet@outlook.com>
2023-10-03 18:32:48 +0300

    Queries for Fact CD4

---
+++ Load Fact CD4/src/main/resources/LoadCD4s.sql
@@ -0,0 +1,14 @@
+select
+    distinct baselines.PatientIDHash,
+    baselines.PatientPKHash,
+    baselines.SiteCode,
+    CD4atEnrollment,
+    CD4atEnrollment_Date as CD4atEnrollmentDate,
+    bCD4 as BaselineCD4,
+    bCD4Date as BaselineCD4Date,
+    lastCD4 AS LastCD4,
+    LastCD4AfterARTStart_Date as LastCD4Date,
+    datediff(yy, patient.DOB, last_encounter.LastEncounterDate) as AgeLastVisit
+from DataExchange.dbo.CT_PatientBaselines as baselines
+left join ODS.dbo.CT_Patient as patient on patient.PatientPK = baselines.PatientPK and patient.SiteCode = baselines.SiteCode
+left join ODS.dbo.Intermediate_LastPatientEncounter as last_encounter on last_encounter.PatientPK = baselines.PatientPK and last_encounter.SiteCode = baselines.SiteCode
