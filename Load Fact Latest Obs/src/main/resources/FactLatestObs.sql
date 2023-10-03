8095c0f8eb5e642b1062cca17b016628a0ccee97
cbrianbet <cbrianbet@outlook.com>
2023-10-03 18:59:53 +0300

    Updated latest obs

--- Load Fact Latest Obs/src/main/resources/FactLatestObs.sql
+++ Load Fact Latest Obs/src/main/resources/FactLatestObs.sql
@@ -5,20 +5,20 @@ select
     agency.AgencyKey,
     age_group.AgeGroupKey,
     diff_care.DifferentiatedCareKey,
-    combined_table.LatestHeight,
-    combined_table.LatestWeight,
-    combined_table.AgeLastVisit,
-    combined_table.Adherence,
-    combined_table.DifferentiatedCare,
-    combined_table.onMMD,
-    combined_table.StabilityAssessment,
-    combined_table.Pregnant,
+    obs.LatestHeight,
+    obs.LatestWeight,
+    obs.AgeLastVisit,
+    obs.Adherence,
+    obs.DifferentiatedCare,
+    obs.onMMD,
+    obs.StabilityAssessment,
+    obs.Pregnant,
     current_date() as LoadDate
-from combined_table
-    left join DimPatient as patient on patient.PatientPKHash = combined_table.PatientPKHash and patient.SiteCode = combined_table.SiteCode
-    left join DimFacility as facility on facility.MFLCode = combined_table.SiteCode
-    left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = combined_table.SiteCode
+from obs
+    left join DimPatient as patient on patient.PatientPKHash = obs.PatientPKHash and patient.SiteCode = obs.SiteCode
+    left join DimFacility as facility on facility.MFLCode = obs.SiteCode
+    left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = obs.SiteCode
     left join DimPartner as partner on partner.PartnerName = upper(MFL_partner_agency_combination.SDP)
     left join DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
-    left join DimAgeGroup as age_group on age_group.Age = combined_table.AgeLastVisit
-    left join DimDifferentiatedCare as diff_care on diff_care.DifferentiatedCare = combined_table.DifferentiatedCare
+    left join DimAgeGroup as age_group on age_group.Age = obs.AgeLastVisit
+    left join DimDifferentiatedCare as diff_care on diff_care.DifferentiatedCare = obs.DifferentiatedCare
