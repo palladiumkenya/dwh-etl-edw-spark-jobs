a184c6cb2068dd131c5b9954e67062e57f77ec1b
cbrianbet <cbrianbet@outlook.com>
2023-10-04 14:06:19 +0300

    Updated fact OVC

--- Load Fact OVC/src/main/resources/SourceOVC.sql
+++ Load Fact OVC/src/main/resources/SourceOVC.sql
@@ -1,19 +1,15 @@
 select
-    distinct ovc.PatientPK,
-             ovc.PatientID,
-             ovc.PatientPKHash,
+    distinct row_number () over (partition by ovc.SiteCode, ovc.PatientPK order by ovc.OVCEnrollmentDate desc) as rank,ovc.PatientPKHash,
              ovc.PatientIDHash,
              ovc.SiteCode,
              OVCEnrollmentDate,
              RelationshipToClient,
              EnrolledinCPIMS,
-             CPIMSUniqueIdentifier,
+             CPIMSUniqueIdentifierHash,
              PartnerOfferingOVCServices,
              OVCExitReason,
              ExitDate,
              datediff(yy, patient.DOB, last_encounter.LastEncounterDate) as AgeLastVisit
 from dbo.CT_OVC as ovc
-         left join dbo.Intermediate_LastPatientEncounter as last_encounter on last_encounter.PatientPK = ovc.PatientPK
-    and last_encounter.SiteCode = ovc.SiteCode
-         left join dbo.CT_Patient as patient on patient.PatientPK = ovc.PatientPK
-    and patient.SiteCode = ovc.SiteCode
+    left join dbo.Intermediate_LastPatientEncounter as last_encounter on last_encounter.PatientPK = ovc.PatientPK and last_encounter.SiteCode = ovc.SiteCode
+    left join dbo.CT_Patient as patient on patient.PatientPK = ovc.PatientPK and patient.SiteCode = ovc.SiteCode
