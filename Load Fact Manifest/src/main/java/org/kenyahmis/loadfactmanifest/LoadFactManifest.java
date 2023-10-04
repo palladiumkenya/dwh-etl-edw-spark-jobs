acf38477aedd326cc3d0dd6ce6ab14eaa3c344bd
cbrianbet <cbrianbet@outlook.com>
2023-10-04 14:49:32 +0300

    Created Fact Manifest

---
+++ Load Fact Manifest/src/main/java/org/kenyahmis/loadfactmanifest/LoadFactManifest.java
@@ -0,0 +1,125 @@
+package org.kenyahmis.loadfactmanifest;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.*;
+import org.apache.spark.storage.StorageLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static org.apache.spark.sql.functions.*;
+
+public class LoadFactManifest {
+    private static final Logger logger = LoggerFactory.getLogger(LoadFactManifest.class);
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf();
+        conf.setAppName("Load Manifest Fact");
+
+        SparkSession session = SparkSession.builder()
+                .config(conf)
+                .getOrCreate();
+
+        RuntimeConfig rtConfig = session.conf();
+
+        Dataset<Row> dimDateDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", "dbo.DimDate")
+                .load();
+        dimDateDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimDateDataFrame.createOrReplaceTempView("DimDate");
+
+        Dataset<Row> dimFacilityDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", "dbo.DimFacility")
+                .load();
+        dimFacilityDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimFacilityDataFrame.createOrReplaceTempView("facility");
+
+        Dataset<Row> dimPartnerDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", "dbo.DimPartner")
+                .load();
+        dimPartnerDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimPartnerDataFrame.createOrReplaceTempView("partner");
+
+        Dataset<Row> dimAgencyDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", "dbo.DimAgency")
+                .load();
+        dimAgencyDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimAgencyDataFrame.createOrReplaceTempView("agency");
+
+        Dataset<Row> dimMFLPartnerAgencyCombinationDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.ods.url"))
+                .option("driver", rtConfig.get("spark.ods.driver"))
+                .option("user", rtConfig.get("spark.ods.user"))
+                .option("password", rtConfig.get("spark.ods.password"))
+                .option("query", "select MFL_Code,SDP,[SDP_Agency] as Agency from dbo.All_EMRSites")
+                .load();
+        dimMFLPartnerAgencyCombinationDataFrame.persist(StorageLevel.DISK_ONLY());
+        dimMFLPartnerAgencyCombinationDataFrame.createOrReplaceTempView("mfl_partner_agency_combination");
+
+        Dataset<Row> uploadsDataFrame = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.ods.url"))
+                .option("driver", rtConfig.get("spark.ods.driver"))
+                .option("user", rtConfig.get("spark.ods.user"))
+                .option("password", rtConfig.get("spark.ods.password"))
+                .option("query", "select cast(DateRecieved as date) as Dateuploaded, cast([Start] as date) as [Start], cast([End] as date) as [End], SiteCode from dbo.CT_FacilityManifest")
+                .load();
+        uploadsDataFrame.persist(StorageLevel.DISK_ONLY());
+        uploadsDataFrame.createOrReplaceTempView("Uploads");
+
+        final String queryFileName = "LoadFactManifest.sql";
+        String query;
+        InputStream inputStream = LoadFactManifest.class.getClassLoader().getResourceAsStream(queryFileName);
+        if (inputStream == null) {
+            logger.error(queryFileName + " not found");
+            return;
+        }
+        try {
+            query = IOUtils.toString(inputStream, Charset.defaultCharset());
+        } catch (IOException e) {
+            logger.error("Failed to load query from file", e);
+            return;
+        }
+        Dataset<Row> manifestDf = session.sql(query);
+        manifestDf = manifestDf.withColumn("FactKey", monotonically_increasing_id().plus(1));
+        manifestDf.printSchema();
+        int numberOfPartitionsBeforeRepartition = manifestDf.rdd().getNumPartitions();
+        logger.info("Number of partitions before repartition: "+ numberOfPartitionsBeforeRepartition);
+        final int writePartitions = 100;
+        manifestDf
+                .repartition(writePartitions)
+                .write()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", "dbo.FactManifiest")
+                .option("truncate", "true")
+                .mode(SaveMode.Overwrite)
+                .save();
+    }
+}
