e8bba80687b81052dc987d46548607946c42882d
cbrianbet <cbrianbet@outlook.com>
2023-10-09 12:14:45 +0300

    Created Vaccination Status Dimension

---
+++ Load Dim Vaccination Status/src/main/java/org/kenyahmis/loaddimvaccinationstatus/LoadDimVaccinationStatus.java
@@ -0,0 +1,60 @@
+package org.kenyahmis.loaddimvaccinationstatus;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.expressions.Window;
+import org.apache.spark.sql.expressions.WindowSpec;
+import org.apache.spark.storage.StorageLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.spark.sql.functions.row_number;
+
+public class LoadDimVaccinationStatus {
+    private static final Logger logger = LoggerFactory.getLogger(LoadDimVaccinationStatus.class);
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf();
+        conf.setAppName("Load Vaccination Status Dimension");
+
+        SparkSession session = SparkSession.builder()
+                .config(conf)
+                .getOrCreate();
+
+        RuntimeConfig rtConfig = session.conf();
+
+        // Vaccination status source
+        logger.info("Loading Vaccination Status source");
+        Dataset<Row> vaccinationStatusSourceDataDf = session.read()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.ods.url"))
+                .option("driver", rtConfig.get("spark.ods.driver"))
+                .option("user", rtConfig.get("spark.ods.user"))
+                .option("password", rtConfig.get("spark.ods.password"))
+                .option("query", "select\n" +
+                        "distinct VaccinationStatus\n" +
+                        "from ODS.dbo.CT_Covid\n" +
+                        "where VaccinationStatus <> '' and VaccinationStatus is not null")
+                .load();
+        vaccinationStatusSourceDataDf.persist(StorageLevel.DISK_ONLY());
+        vaccinationStatusSourceDataDf.createOrReplaceTempView("source_vaccination_status");
+        vaccinationStatusSourceDataDf.printSchema();
+
+        // load Vaccination status Dim
+        Dataset<Row> dimVaccinationStatus = session.sql("select source_vaccination_status.*, current_date() as LoadDate " +
+                "from source_vaccination_status");
+
+        WindowSpec window = Window.orderBy("VaccinationStatus");
+        dimVaccinationStatus = dimVaccinationStatus.withColumn("VaccinationStatusKey",  row_number().over(window));
+        dimVaccinationStatus
+                .write()
+                .format("jdbc")
+                .option("url", rtConfig.get("spark.edw.url"))
+                .option("driver", rtConfig.get("spark.edw.driver"))
+                .option("user", rtConfig.get("spark.edw.user"))
+                .option("password", rtConfig.get("spark.edw.password"))
+                .option("dbtable", "dbo.DimVaccinationStatus")
+                .mode(SaveMode.Overwrite)
+                .save();
+    }
+}
